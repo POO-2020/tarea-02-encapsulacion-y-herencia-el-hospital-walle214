8ab87c91314020f1e071ec12426fa3143ea28fde
Ulises Ramirez <ulisesraf.ramirez@gmail.com>
2020-03-17 17:30:39 -0600

    Clase paciente recive los parametros mediante un objeto

--- modulos/Paciente.js
+++ modulos/Paciente.js
@@ -1,13 +1,5 @@
-import Nombre from "./Nombre.js";
-import Fecha from "./Fecha.js";
 export default class Paciente {
-    /**
-     * 
-     * @param {Nombre} nombre 
-     * @param {Fecha} fechaNacimiento
-     * @param {Number} telefono 
-     */
-    constructor(nombre, fechaNacimiento, telefono) {
+    constructor({nombre, fechaNacimiento, telefono}) {
         this._nombre = nombre;
         this._fechaNacimiento = fechaNacimiento;
         this._telefono = telefono;
