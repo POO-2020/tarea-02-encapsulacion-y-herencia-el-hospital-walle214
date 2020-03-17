547018880072882a291e59852fef581e0b49ff25
Ulises Ramirez <ulisesraf.ramirez@gmail.com>
2020-03-17 17:31:54 -0600

    Clase cita recive los parametros mediante un objeto

--- modulos/Cita.js
+++ modulos/Cita.js
@@ -1,17 +1,5 @@
-import Fecha from "./Fecha.js";
-import Tiempo from "./Tiempo.js";
-import Doctor from "./Doctor.js";
-import Paciente from "./Paciente.js";
-
 export default class Cita {
-    /**
-     * 
-     * @param {Fecha} fecha Fecha de la
-     * @param {Tiempo} hora Hora de la cita
-     * @param {Doctor} doctor Doctor que atiende la cita
-     * @param {Paciente} paciente Paciente que pidió la cita
-     */
-    constructor(fecha, hora, doctor, paciente) {
+    constructor({fecha, hora, doctor, paciente}) {
         this._fecha = fecha;
         this._hora = hora;
         this._doctor = doctor;
