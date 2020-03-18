feded6865c065c519c795d69f2ae0a0a898888df
Ulises Ramirez <ulisesraf.ramirez@gmail.com>
2020-03-17 18:12:37 -0600

    Metodo para compararlo con otra cita

--- modulos/Doctor.js
+++ modulos/Doctor.js
@@ -1,11 +1,13 @@
 export default class Doctor {
-    constructor({nombre, especialidad, telefono, cedula}) {
+    constructor({ nombre, especialidad, telefono, cedula }) {
         this._nombre = nombre;
         this._especialidad = especialidad;
         this._telefono = telefono;
         this._cedula = cedula;
     }
-    getApellido = _ => this.nombre.getApellido();
-    getPerfil = _ => `Cedula: ${this._cedula}\nEspecialidad: ${this._especialidad}\nNombre ${this._nombre.getNombreCompleto()}\nTelefono: ${this._telefono}`;
+    esIgual = doctor => this.getPerfil() == doctor.getPerfil();
 
+    getApellido = _ => this.nombre.getApellido();
+
+    getPerfil = _ => `Cedula: ${this._cedula}\nEspecialidad: ${this._especialidad}\nNombre ${this._nombre.getNombreCompleto()}\nTelefono: ${this._telefono}`;
 }
